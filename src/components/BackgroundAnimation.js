b3b776bd7386d690d8e2ce5504f60deac2828a0e
Dylan Cheung <cheund3@gmail.com>
2020-03-12 09:21:33 -0400

    messing w three.js examples

--- src/components/BackgroundAnimation.js
+++ src/components/BackgroundAnimation.js
@@ -1,40 +1,42 @@
 import React, { Component } from 'react'
 import * as THREE from 'three'
 import '../styles/BackgroundAnimation.css'
+import ReactDOM from 'react-dom'
+
 
 export default class BackgroundAnimation extends Component {
 
     componentDidMount(){
-        var camera, scene, renderer;
-        var geometry, material, mesh;
+        // var camera, scene, renderer;
+        // var geometry, material, mesh;
         
     
-        camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-        camera.position.z = 5;
+        // camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+        // camera.position.z = 5;
     
-        scene = new THREE.Scene();
+        // scene = new THREE.Scene();
     
-        geometry = new THREE.BoxGeometry( 1,1,1 );
-        material = new THREE.MeshNormalMaterial();
+        // geometry = new THREE.BoxGeometry( 1,1,1 );
+        // material = new THREE.MeshNormalMaterial();
     
-        mesh = new THREE.Mesh( geometry, material );
-        scene.add( mesh );
+        // mesh = new THREE.Mesh( geometry, material );
+        // scene.add( mesh );
     
-        renderer = new THREE.WebGLRenderer( { antialias: true } );
-        renderer.setSize( window.innerWidth, window.innerHeight-4 );
-        this.mount.appendChild( renderer.domElement );
+        // renderer = new THREE.WebGLRenderer( { antialias: true } );
+        // renderer.setSize( window.innerWidth, window.innerHeight-4 );
+        // this.mount.appendChild( renderer.domElement );
         
-        function animate() {
+        // function animate() {
         
-            requestAnimationFrame( animate );
+        //     requestAnimationFrame( animate );
         
-            mesh.rotation.x += 0.01;
-            mesh.rotation.y += 0.02;
+        //     mesh.rotation.x += 0.01;
+        //     mesh.rotation.y += 0.02;
         
-            renderer.render( scene, camera );
+        //     renderer.render( scene, camera );
         
-        }
-        animate();
+        // }
+        // animate();        
     }
 
     render() {
