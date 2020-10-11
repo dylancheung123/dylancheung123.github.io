625a80d9db10d44c0206b8f67ae9d2a0ba485931
Dylan Cheung <dylancheung123@gmail.com>
2020-10-11 13:24:55 -0400

    adding a router and moving towards creating reusable components for displaying projects

---
+++ src/components/card.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default class Card extends React.Component {
+  render() {
+    const { title, description, thumbnail, onClick } = this.props
+    if ( !title || !description || !thumbnail || !onClick ) return
+    return (
+      <div className='card'>
+        <div className='title'>{title}</div>
+      </div>
+    )
+  }
+}
--- src/views/Projects.js
+++ src/views/Projects.js
@@ -4,7 +4,7 @@ export default class Projects extends React.Component {
     render(){
         return(
             <div className="">
-                Projects
+                
             </div>
         )
     }
