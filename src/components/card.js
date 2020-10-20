1245c6a49b5dd26010756695f06d934d55a352d2
Dylan Cheung <dylancheung123@gmail.com>
2020-10-20 19:53:41 -0400

    temp

--- src/components/card.js
+++ src/components/card.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default class Card extends React.Component {
   render() {
     const { title, description, thumbnail, onClick } = this.props
-    if ( !title || !description || !thumbnail || !onClick ) return
+    if ( !title || !description || !thumbnail ) return
     return (
       <div className='card'>
         <div className='title'>{title}</div>
--- src/views/Photography.js
+++ src/views/Photography.js
@@ -1,11 +1,24 @@
 import React from 'react'
+import Card from '../components/Card'
 
 export default class Photography extends React.Component {
-    render(){
-        return(
-            <div className="">
-                
-            </div>
-        )
+  render(){
+    const cardA = { 
+      title: 'Title',
+      description: 'Lorem ipsum dolor sit amet, hinc deleniti tractatos vis id, te inermis maiestatis vel. Et honestatis neglegentur intellegebat sea, justo omnium constituto vim an. Vel deleniti erroribus conclusionemque an, ornatus democritum instructior sea at, pro detracto laboramus assentior in. His ad purto erant democritum. Veri adhuc accusam eu mea, ut pro case ceteros incorrupte.',
+      thumbnail: ''
     }
+    const cards = [  cardA, cardA, cardA, cardA, cardA, cardA, ]
+    return(
+      <div className="photography-container">
+        {
+          cards.map( card => {
+            return (
+              <Card title={cardA.title} description={card.description} thumbnail={card.thumbnail}/>
+            )
+          })
+        }
+      </div>
+    )
+  }
 }
