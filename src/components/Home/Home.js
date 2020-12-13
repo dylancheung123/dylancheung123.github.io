4bfb631ec40f25af2c118afa7f114995ef4f2110
dylan <cheund3@gmail.com>
2020-12-13 10:38:54 -0500

    Fixing indentation on Home file

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,29 +1,28 @@
 import React from 'react'
 
 export default class Home extends React.Component {
-    render(){
-        return(
-            <div className="page">
-                <div className="container">
-                    <div className="bio-header">
-                        <div className="title">
-                            Hi, I'm Dylan
-                        </div>
-                    </div>
-                    
-                    <div className="center">
-                        <div className="bio">
-                            <p>
-                                I'm full-stack web developer at <a href="https://himarley.com" target="_blank" className="himarley_link"> HiMarley </a> 
-                                where we create simple and intuitive tools to improve the workflow of insurance operators.
-                            </p>
-                            <p>
-                                I enjoy anything technology related and I strive to keep learning
-                            </p>
-                        </div>
-                    </div>
-                </div>
+  render(){
+    return(
+      <div className="page">
+        <div className="container">
+          <div className="bio-header">
+            <div className="title">
+                Hi, I'm Dylan
             </div>
-        )
-    }
+          </div>
+          <div className="center">
+            <div className="bio">
+              <p>
+                I'm full-stack web developer at <a href="https://himarley.com" target="_blank" className="himarley_link"> HiMarley </a> 
+                where we create simple and intuitive tools to improve the workflow of insurance operators.
+              </p>
+              <p>
+                I enjoy anything technology related and I strive to keep learning
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
