10b87d9607c604552351a4a0d51b76a6e80052c1
Dylan Cheung <dylancheung123@gmail.com>
2025-11-03 21:29:39 -0800

    favicon

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,20 @@ import { createRoot } from 'react-dom/client';
 import { HashRouter, Routes, Route } from 'react-router-dom';
 import { Home } from './pages/Home';
 import './styles.css';
+import faviconUrl from './favicon.svg';
+
+// Set favicon programmatically
+const setFavicon = () => {
+  let link = document.querySelector("link[rel~='icon']") as HTMLLinkElement;
+  if (!link) {
+    link = document.createElement('link');
+    link.rel = 'icon';
+    document.getElementsByTagName('head')[0].appendChild(link);
+  }
+  link.href = faviconUrl;
+};
+
+setFavicon();
 
 const container = document.getElementById('root');
 if (!container) {
